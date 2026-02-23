c9faf062381cd4cfb917da9f24dba6288ca2b0f7
Joe Fearnley <joe.fearnley@gmail.com>
2026-02-22 21:31:02 -0500

    work in progress

--- 2025/go/day1/main.go
+++ 2025/go/day1/main.go
@@ -12,45 +12,74 @@ func check(e error) {
 	}
 }
 
+func AbsInt(x int) int {
+	if x < 0 {
+		return -x
+	}
+	return x
+}
+
 func main() {
 
-	input, err := os.Open("input")
+	input, err := os.Open("input.txt")
 	check(err)
 
 	scanner := bufio.NewScanner(input)
 
 	currentDial := 50
 	password := 0
+	lineNumber := 0
 
 	for scanner.Scan() {
-		line := scanner.Text()
+		line := string(scanner.Text())
 
-		// split line into array of strings
-		// check if first character is an "R or L"
-		rightOrLeft := line[0]
+		rightOrLeft := string(line[0])
 		distance, err := strconv.Atoi(line[1:])
 		check(err)
 
-		if rightOrLeft == 'L' {
-			difference := currentDial - distance
+		if rightOrLeft == "L" {
 
-			if difference < 0 {
-				dial = 99 - difference
-			} else {
-				dial = dial + clicks
+			for value := range distance {
+				currentDial = currentDial - int(value)
+
+				if currentDial == -1 {
+					currentDial = 99
+				}
 			}
 		} else {
-			difference := dial + clicks
+			for value := range distance {
+				currentDial = currentDial + int(value)
 
-			if difference < 0 {
-				dial = 99 - difference
-			} else {
-				dial = dial + clicks
+				if currentDial > 100 {
+					currentDial = currentDial - 100
+				}
 			}
 		}
 
-		if dial == 0 {
+		if currentDial == 0 {
 			password++
 		}
+
+		if currentDial > 100 {
+
+			println("\n\n----------------------")
+			println("current dial: " + strconv.Itoa(currentDial))
+			println("distance: " + strconv.Itoa(distance))
+			println("rightOrLeft: " + rightOrLeft)
+			println("line: " + line)
+			println("line number: " + strconv.Itoa(lineNumber))
+			println("----------------------\n\n")
+
+			os.Exit(1)
+		}
+
+		println("current dial: " + strconv.Itoa(currentDial))
+
+		lineNumber++
 	}
+
+	println("password: " + strconv.Itoa(password))
+	print("")
+
+	input.Close()
 }
